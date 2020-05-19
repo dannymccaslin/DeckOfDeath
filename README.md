26e6018c024d9374add3bb7dfc01d8c4da3d8c05
Danny McCaslin <danny.mccaslin@frederickwater.com>
2020-05-19 10:25:17 -0400

    Added a feature to time exercises

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#DeckOfDeath 
+# DeckOfDeath
+
+## Version 1.1 -2020-05-19
+Added a feature that lets you see the time you spent on each card, each suit, and the total time of each workout after clicking Finish.
+
+## Version 1.0 -2020-05-13
+Initial version
--- script.js
+++ script.js
@@ -8,7 +8,12 @@ let startButton = document.getElementById('start_button');
 let cardsLeft = document.getElementById('cards_left');
 let exercise = document.getElementById('exercise');
 let reload = document.getElementById('reload_button');
+let finish_button = document.getElementById('finish_button');
 let inputs = document.getElementById('inputs');
+let startTime;
+let stopTime;
+let d;
+let cardTimer;
 let workout = {
     spades: '',
     clubs: '',
@@ -65,9 +70,25 @@ function Card(s,f)  {
     }
 }
 
+function Times (card,suit,time) {
+    this.card = card;
+    this.suit = suit;
+    this.totalTime = time;
+    this.getCard = () => {
+        return this.card;
+    };
+    this.getSuit = () => {
+        return this.suit;
+    };
+    this.getCardTime = () => {
+        return this.totalTime;
+    };
+}
+
 var deck = []
 var suits = ['Hearts','Spades','Clubs','Diamonds'];
 var faces = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
+var deckTimes = [];
 
 //Create the deck by creating each card and adding it to the deck
 for (var i = 0; i < suits.length; i++) {
@@ -86,13 +107,42 @@ for (var i = 0; i < suits.length; i++) {
 
     }
 }
+function n(n){
+    return n > 9 ? "" + n: "0" + n;
+}
+
+var convertTime = (ms) => {
+    // 1- Convert to seconds:
+    var seconds = ms / 1000;
+    // 2- Extract hours:
+    var hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    var minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = seconds % 60;
+    return `${n(hours)}:${n(minutes)}:${n(seconds.toFixed(2))}`
+}
+
+
 
 var handleCards = () => {
     cardsLeft.innerText = `Cards Left: ${deck.length-1}`;
     carddiv.innerHTML = `<img id="cardImg" src="images/${deck[0].link}.png" alt="${deck[0].card}"</img>`;
     var cardSuit = deck[0].suit.toLowerCase();
     exercise.innerHTML = `<b>${deck[0].value} ${workout[cardSuit]}</b>`;
+    d = new Date();
+    startTime = d.getTime();
 
+}
+
+var collectTime = () => {
+    d = new Date();
+    stopTime = d.getTime();
+    cardTimer = stopTime - startTime;
+    console.log("Card Timer", cardTimer);
+    var newTime = new Times(deck[0].card, deck[0].suit,cardTimer);
+    deckTimes.push(newTime);
 }
 
 
@@ -106,12 +156,15 @@ startButton.onclick = () => {
     workout.clubs = clubInput.value;
     shuffle(deck);
     handleCards();
+    startButton.classList.add('hidden');
+    finish_button.classList.remove('hidden');
     
 
 }
 
 
 nextCard.onclick = () => {
+    collectTime();
     deck.shift();
     handleCards();
     if (deck.length-1 == 0) {
@@ -124,3 +177,46 @@ reload.onclick = () => {
     location.reload();
 }
 
+finish_button.onclick = () => {
+    nextCard.classList.add('hidden');
+    reload.classList.remove('hidden');
+    collectTime();
+    var spadesTime = 0;
+    var clubsTime = 0;
+    var diamondsTime = 0;
+    var heartsTime = 0;
+    let finalTime = 0;
+    carddiv.innerHTML = '<ul id="finallist"></ul>';
+    var finalList = document.getElementById('finallist');
+    for (var i = 0; i < deckTimes.length; i++ ) {
+        if (deckTimes[i].getSuit() == "Spades" || deckTimes[i].getSuit() == "Clubs"){
+            finalList.innerHTML +=`<li class="blackCard">${deckTimes[i].getCard()} --- ${convertTime(deckTimes[i].getCardTime())}</li>`
+        } else {
+            finalList.innerHTML +=`<li class="redCard">${deckTimes[i].getCard()} --- ${convertTime(deckTimes[i].getCardTime())}</li>`
+        }
+        finalTime += deckTimes[i].getCardTime();
+        switch(deckTimes[i].getSuit()) {
+            case 'Spades':
+                spadesTime += deckTimes[i].getCardTime();
+                break;
+            case 'Clubs':
+                clubsTime += deckTimes[i].getCardTime();
+                break;
+            case 'Diamonds':
+                diamondsTime += deckTimes[i].getCardTime();
+                break;
+            case 'Hearts':
+                heartsTime += deckTimes[i].getCardTime();
+                break;
+        };
+        
+    }
+    finalList.innerHTML += `<li class="blackCard summary">Total --- ${convertTime(finalTime)}</li>`;
+    finalList.innerHTML += `<li class="listheading">Suits</li>`
+    finalList.innerHTML += `<li class="blackCard"><span class="blackicons">&spades;</span> Spades -- ${workout.spades} --- ${convertTime(spadesTime)}</li>`;
+    finalList.innerHTML += `<li class="blackCard"><span class="blackicons">&clubs;</span> Clubs -- ${workout.clubs} --- ${convertTime(clubsTime)}</li>`;
+    finalList.innerHTML += `<li class="redCard"><span class="whiteicons">&diams;</span> Diamonds -- ${workout.diamonds} --- ${convertTime(diamondsTime)}</li>`;
+    finalList.innerHTML += `<li class="redCard"><span class="whiteicons">&hearts;</span> Hearts --${workout.hearts} --- ${convertTime(heartsTime)}</li>`;
+
+
+}
